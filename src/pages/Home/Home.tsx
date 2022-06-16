bf12f6d35ace15ddff1ee94bbf81b6312445a6c7
bartosz121 <bmagiera121@gmail.com>
2022-06-16 13:55:02 +0200

    Add Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export const Home = (props: Props) => {
+  return (
+    <>
+      <h2 className="text-xl text-primary">Home</h2>
+    </>
+  );
+};
