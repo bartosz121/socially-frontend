e322241bafcdf68a596c17146cf30445b40e2f6c
bartosz121 <bmagiera121@gmail.com>
2022-06-16 13:43:39 +0200

    Add custom animations and theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,42 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.2s ease-in",
+        // as of right now you cant configure dismiss time of a toast, its hardcoded to 1s
+        // thats why fadeOut must be 1s otherwise toast appears again after fade out
+        // TODO change fadeOut time to less after they merge https://github.com/timolins/react-hot-toast/pull/89
+        fadeOut: "fadeOut 1s ease-out",
+      },
+
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        }
+      }
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cmyk", "cupcake"],
+    themes: [{
+      socially: {
+        "primary": "#3f45fc",
+        "secondary": "#F2D7EE",
+        "accent": "#bab1ef",
+        "neutral": "#222D3F",
+        "base-100": "#EDF0F3",
+        "base-300": "#F7F7F7",
+        "info": "#65A1DC",
+        "success": "#5BECC5",
+        "warning": "#F6A037",
+        "error": "#FB4B54",
+      },
+    }, "halloween"],
   }
 }
