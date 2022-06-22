1515640059bc05032dc4c7ab88934a115d074aaa
bartosz121 <bmagiera121@gmail.com>
2022-06-22 17:54:03 +0200

    Add htmlattributes to props interface

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-type Props = {};
+interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
-const Spinner = (props: Props) => {
+const Spinner = ({ className, ...props }: Props) => {
   return (
     <div className="overflow-hidden">
       <div
         style={{ borderTopColor: "transparent" }}
-        className="w-16 h-16 border-4 border-primary border-solid rounded-full animate-spin"
+        className={`w-16 h-16 border-4 border-primary border-solid rounded-full animate-spin ${className}`}
+        {...props}
       ></div>
     </div>
   );
