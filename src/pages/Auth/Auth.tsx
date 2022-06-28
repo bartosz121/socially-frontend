e1c9c503da3ded2225866c82968dc583944b3284
bartosz121 <bmagiera121@gmail.com>
2022-06-28 20:27:51 +0200

    Add Auth page

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+
+import { useAuth } from "../../services/auth.serivce";
 
 type Props = {};
 
 const Auth = (props: Props) => {
-  return (
-    <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-      <div className="max-w-lg mx-auto">
-        <Outlet />
+  const { user } = useAuth();
+  const navigator = useNavigate();
+
+  if (user) {
+    navigator("/", { replace: true });
+    return <></>;
+  } else {
+    return (
+      <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+        <div className="max-w-lg mx-auto">
+          <Outlet />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Auth;
